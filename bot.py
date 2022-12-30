fbed3931d79e8d9ae4ea44d425a3befa4c4f9bbb
EpiclyDelta <93168002+EpiclyDelta@users.noreply.github.com>
2022-12-30 15:37:39 -0500

    Fixing Bugs.
    There will probably be a lot of commits, because of this, but there were bugs i was not aware of.

--- bot.py
+++ bot.py
@@ -254,6 +254,8 @@ async def checkmood(ctx):
         await asyncio.sleep(2)
     if MadnessLevel > 2:
         await ctx.respond(f"He is calm, and happy! Madness level = {MadnessLevel}")
+    elif MadnessLevel < 0:
+        await ctx.respond(f"He is calm, and happy! Madness level = {MadnessLevel}")
     elif MadnessLevel > 4:
         await ctx.respond(f"He is sort of calm, and not really that happy. Madness level = {MadnessLevel}")
     elif MadnessLevel > 6:
@@ -261,6 +263,7 @@ async def checkmood(ctx):
     elif MadnessLevel > 8:
         await ctx.repsond(f"He is annoyed. Getting really close to getting mad at anyone. Madness level = {MadnessLevel}")
     elif MadnessLevel > 10:
-        await ctx.respond(f"He is Mad, Calm him before interacting with him. Madness Level = {MadnessLevel}")   
+        await ctx.respond(f"He is Mad, Calm him before interacting with him. Madness Level = {MadnessLevel}")
+   
 
 bot.run('[Your Token Goes Here]')
