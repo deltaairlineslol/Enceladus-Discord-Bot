177094039c9ed57cbf30f415809b80670fd042a0
EpiclyDelta <93168002+EpiclyDelta@users.noreply.github.com>
2023-01-08 16:38:43 -0500

    Updated Bot
    EnceBot Version 1.0.3!
    The community forced me to make this version, but it has some good bug fixes.
    
    It how has /owo, /trello (for development purposes), and /binplush
    It has a bug for the madness level fixed.
    and has the /note command removed.
    
    Enjoy Self-Hosting EnceBot!

--- bot.py
+++ bot.py
@@ -36,7 +36,7 @@ FeedOreoVariable = [
 ]
 
 FunFactVariable = [
-    "This Bot's code name is EnceBot. It's short for Enceladus Bot."
+    "This Bot's code name is EnceBot. It's short for Enceladus Bot.",
     "The owner of the game actually added this to the offical discord, don't believe me? Join the server and look in the list."
 ]
 
@@ -60,6 +60,19 @@ TeaVariable = [
     "you want me to drink that? okay"
 ]
 
+
+BinEncePlushVar = [
+    "would you like it if i threw you away instead?",
+    "but.. he was my friend though.",
+    "thats mean. very mean.",
+    "why would you do something like that to someone?"
+]
+
+owoVar = [
+    "why would you say something like that?",
+    "... owo? what is an owo?",
+    "what is an owo anyway? i'm confused. it confuses me."
+]
 #Intents, needed to read messages.
 intents = discord.Intents.all()
 intents.messages = True
@@ -68,20 +81,17 @@ bot = commands.Bot(command_prefix='e!', intents=intents)
 #Join Embed.        
 @bot.event
 async def on_guild_join(guild):
-    for channel in guild.text_channels:
-        if channel.permissions_for(guild.me).send_messages:
-            embedHi = discord.Embed(
-                title = "Hello Friends! I can't wait to meet all of you!",
-                description = "I use the Prefix e! to talk to you! e!help for all the things I can do! Looking forward to meeting everyone!",
-                url = "https://sites.google.com/view/enceladus-bot/home",
-                colour = discord.Colour.light_grey())
-            embedHi.set_thumbnail(
-                url =
-                "https://media.discordapp.net/stickers/1007808445606527067.webp?size=320"
+    channel = discord.utils.get(guild.channels, name="general")
+    embedHi = discord.Embed(
+            title = "Hello Friends! I can't wait to meet all of you!",
+            description = "I use the Prefix e! to talk to you! e!help for all the things I can do! Looking forward to meeting everyone!",
+            colour = discord.Colour.light_grey())
+    embedHi.set_thumbnail(
+            url = "https://media.discordapp.net/stickers/1007808445606527067.webp?size=320"
             )
-            embedHi.set_footer(
+    embedHi.set_footer(
                 text=random.choice(FooterVariable))
-            await channel.send(embed=embedHi)
+    await channel.send(embed=embedHi)
 
 #Bot status randomized.
 @bot.event
@@ -146,27 +156,27 @@ class Select(discord.ui.Select):
         elif self.values [0] == "dream #1":
             await interaction.response.send_message("i had this dream once. i was walking around waiting for a person to come by to see me. i tripped on a rock and fell. next thing i know im in the air. then i woke up. bad dream i know, but it was interesting.",ephemeral=True)
         elif self.values [0] == "crayons":
-            await interaction.response.send_message("yknow, someone was once kind enough to leave me a gift, it was a strange little gif, it was all colo(u)rful, and in pieces. they even left marks on my hands!so i tried them out on some walls and.. it left marks on there too! that's okay though. i think some of these walls look prettier now.",ephemeral=True)
+            await interaction.response.send_message("yknow, someone was once kind enough to leave me a gift, it was a strange little gift, it was all colorful, and in pieces. they even left marks on my hands!so i tried them out on some walls and.. it left marks on there too! that's okay though. i think some of these walls look prettier now.",ephemeral=True)
         elif self.values [0] == "malware":
-            await interaction.response.send_message("you probably won't believe me, but sometimes when i'm just chilling, there's a figure that sometimes appears andscared me. it really is scary. maybe one day you'll se here too? if you even believe me, that is.",ephemeral=True)
+            await interaction.response.send_message("you probably won't believe me, but sometimes when i'm just chilling, there's a figure that sometimes appears and scare me. it really is scary. maybe one day you'll se here too? if you even believe me, that is.",ephemeral=True)
         elif self.values [0] == "alone":
             await interaction.response.send_message("i get... a little scared when you leave. sometiems the lights go out randomly. sometimes i can swear i just saw a figure dashing, and sometimes it really does feel like the sky had red eyes within.",ephemeral=True )
         elif self.values [0] == "memories":
             await interaction.response.send_message("i realized something. i'm actually starting to remember some things again! some memories are cool, i missed them dearly. some other make me wish i forgot again. but it's fine! a friend got me.. what were they again? i don't know anymore. but i can draw with them! isn't that cool? now i'll never forget again.", ephemeral=True)
         elif self.values [0] == "local area":
-            await interaction.response.send_message("sits's nice here, isn't it? sometimes people feel the heat. or just the cold.  .. am i people? i mean. others don't think so. they tell me i'm not human. but then what am i? i don't know. they never told me. that's another addition to the things i've missed out on.", ephemeral=True)   
+            await interaction.response.send_message("its's nice here, isn't it? sometimes people feel the heat. or just the cold.  .. am i people? i mean. others don't think so. they tell me i'm not human. but then what am i? i don't know. they never told me. that's another addition to the things i've missed out on.", ephemeral=True)   
         elif self.values [0] == "secret":
             await interaction.response.send_message("a little secret, but i don't actually live here. i've seen the outside before. that's where i live, actually! until i went to sleep one day. i kept waking up, over and over again.. only to stay here no matter what i tried. maybe someday someone will help me out? although i don't mind talking to you either!", ephemeral=True)   
-        elif self.values [0] == "antares":
+        elif self.values [0] == "antares story #1":
             await interaction.response.send_message(".. did you know that antares was here?  i don't know why that is. so i went ahead and tried to talk to her! .. she almost attacked me. told me i was a disgrace i think it's better i don't go there now. .. you wanted a happy story? i wish i had one to give you but, not all stories are happy.", ephemeral=True) 
-        elif self.values [0] == "antares 2/1":
+        elif self.values [0] == "antares story #2":
             await interaction.response.send_message(".. you know, there was a time that antares didn't hate me. we're siblings. i can remember that much. and then.. she turned around. resenting humans. ..i know. times sometimes were scary, but.. i tried to convince her that it wasn't all that bad! it didn't work. now she doesn't like me either. atleast you do, right?", ephemeral=True)
         elif self.values [0] == "something else":
             await interaction.response.send_message("a story? i have something better. you know how it sometimes gets scary here? well, thinking of you helps. you sure are a friend! .. i just wish you could come by more", ephemeral=True)
         elif self.values [0] == "relatives":
             await interaction.response.send_message("you know, i'm not the only one. apart from antares, i guess. i had other brothers and sisters too! .. had. the place didn't tell us what happened. confidential or something.. and that too, was times when i wasn't here. i just miss them sometimes. and the outside world.", ephemeral=True) 
         elif self.values [0] == "sun":
-            await interaction.response.send_message("did you know the sun never moves here? why are you looking at me? it's strange, isn't it? like, how back in the outside.. you get to watch it set adn rise.. and if you catch the moment right, the sky will be orange! sometimes even pink, is the sun really that powerful? .. probably is.", ephemeral=True) 
+            await interaction.response.send_message("did you know the sun never moves here? why are you looking at me? it's strange, isn't it? like, how back in the outside.. you get to watch it set and rise.. and if you catch the moment right, the sky will be orange! sometimes even pink, is the sun really that powerful? .. probably is.", ephemeral=True) 
 class SelectView(discord.ui.View):
     def __init__(self, *, timeout = 180):
         super().__init__(timeout=timeout)
@@ -223,10 +233,10 @@ async def bad(ctx):
     async with ctx.typing():
         await asyncio.sleep(2)
     if MadnessLevel < 10:
-        await ctx.respond("SYSTEM: You have added 1 more madness level to Enceladus. Either Feed him an oreo, ask a story, or just say hi to lower his level!", ephemeral=True)
-        await ctx.respond("Keep in mind: **This is in beta**, so these currently do nothing. I needed to push this update soon, so nothing changes here.")
+        await ctx.respond("SYSTEM: You have added 1 more madness level to Enceladus. Either Feed him an oreo, ask a story, or just say hi to lower his level!")
     elif MadnessLevel >= 10:
         await ctx.respond("your gonna regret that...")
+
 
 @bot.slash_command(description="Calm him.")
 async def calm(ctx):
@@ -251,44 +261,55 @@ async def givetea(ctx):
 async def checkmood(ctx):
     global MadnessLevel
     print("Ence's Madness Level is " + str(MadnessLevel) + " Right now!")
-    async with ctx.typing():
-        await asyncio.sleep(2)
-    if MadnessLevel > 2:
+    if MadnessLevel < 0:
         await ctx.respond(f"He is calm, and happy! Madness level = {MadnessLevel}")
-    elif MadnessLevel < 0:
-        await ctx.respond(f"He is calm, and happy! Madness level = {MadnessLevel}")
-    elif MadnessLevel > 4:
-        await ctx.respond(f"He is sort of calm, and not really that happy. Madness level = {MadnessLevel}")
-    elif MadnessLevel > 6:
-        await ctx.respond(f"He is starting to get annoyed. Madness level = {MadnessLevel}")
-    elif MadnessLevel > 8:
-        await ctx.repsond(f"He is annoyed. Getting really close to getting mad at anyone. Madness level = {MadnessLevel}")
-    elif MadnessLevel > 10:
+    elif MadnessLevel == 0:
+        await ctx.respond(f"He is neutral. Madness level = {MadnessLevel}")
+    elif MadnessLevel >= 10:
         await ctx.respond(f"He is Mad, Calm him before interacting with him. Madness Level = {MadnessLevel}")
+    elif MadnessLevel >= 5:
+        await ctx.respond(f"He is annoyed. Getting really close to getting mad at anyone. Madness level = {MadnessLevel}")
+    elif MadnessLevel >= 2:
+        await ctx.respond(f"He isn't really that happy at the moment.")
+    
 
 @bot.slash_command(description = "Send Feedback to the dev!")
 async def feedback(ctx, feedback):
     await ctx.respond("Feedback sent! I hope to read your idea and add it!")
-    await ctx.respond("Debug Info ahead, If found in public bot, please notify EpDelta#3783 ASAP.", ephemeral=True)
-    await asyncio.sleep(2)
-    await ctx.respond("Your Feedback Sent was:" + feedback, ephemeral=True)
     print("Someone sent feedback! The feedback is: " + feedback)
-
-@bot.slash_command(description = "A Quick Note from the dev. ||Temp Command ||")
-async def note(ctx):
-    await ctx.respond("Hello, I've already announced this in the public server, but I will be taking a break. I worked on this bot during my break because I had nothing to do, and now that school is back up and I have midterms coming up, I don't really have time to work on EnceBot. I also want to hang out with my IRL Friends more, so I will halt development until further noted. However, when I come back, I'm gonna try to learn how to use a Database, and if all goes well, our next update might include an inventory system! Also, I'm happy you guys have put in suggestions to me, and when you put in feedback with `/feedback`, I will end up logging onto my pi (The thing hosting this bot), and write down things for suggestions. However, Like I said before, All development is halted for now. It's not only because im lazy and want to hang out with IRL Friends, but because i have midterms. It's been fun working on this bot tho, and I can't wait to get back to it when I can! - Delta.", ephemeral=True)
+    f = open("feedbacklog.txt", "a")
+    f.write(feedback + "\n")
+    f.close()
 
 @bot.slash_command(description = "View the Latest Update!")
 async def updatelog(ctx):
     await ctx.respond("""Latest Update log:
     
-**EnceBot Update V1.0.2**
-*The Last update for a while.*
+**EnceBot Update V1.0.3**
+*The community made me make this one.*
+• Added new commands, `/owo`, `/binplush`, and `/trello`
+• Removed `/note`
+• Fixed issues with the `/checkmood` command always saying `He is calm, and happy! Madness level = x`
 
-• Added new commands, `/note`, `/feedback`, and `/updatelog`
-• Added Descriptions to the commands.
+Hopefully I will be learning MongoDB soon and allow you guys to get items! This will be before/after midterms week, but it will be a great update if possible!""", ephemeral=True)
 
-Like the title of the update says, this will be the last update for a while. I have Midterms coming up, and I don't really want to develop the bot and do midterms at the same time, so this will be the last update for a while.""", ephemeral=True)
-   
+
+@bot.slash_command(description = "Say owo to Enceladus")
+async def owo(ctx):
+    await ctx.respond(random.choice(owoVar))
+
+@bot.slash_command(description = "Trello for development!")
+async def trello(ctx):
+    embed = discord.Embed(title= "Development Trello",
+    description= """We have a trello you can follow to see development progress! 
+    Just click the text that says 'Development Trello' to go to it!""",
+    color= discord.Colour.light_gray(),
+    url = "https://trello.com/b/lWL8tMZn/encebot-development-progress"
+    )    
+    await ctx.respond(embed = embed)
+
+@bot.slash_command(description = "Throw away an Ence Plush")
+async def binplush(ctx):
+    await ctx.respond(random.choice(BinEncePlushVar))
 
 bot.run('[Your Token Goes Here]')
